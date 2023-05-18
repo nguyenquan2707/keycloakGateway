6b2952584df9680da8e10d7996bd957f90c862ee
Quan Nguyen <vozermuonnam1@gmail.com>
2023-05-18 16:07:57 +0700

    get userinfo from resource server

---
+++ backend-gateway-client/src/main/java/com/sergio/auth/backend/client/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.sergio.auth.backend.client.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+//    @GetMapping("/user")
+//    public OAuth2User index(Principal principal) {
+//        OAuth2AuthenticationToken oAuth2AuthenticationToken =
+//                (OAuth2AuthenticationToken) principal;
+//        OAuth2User user = oAuth2AuthenticationToken.getPrincipal();
+//        System.out.println(user);
+//        return user;
+//    }
+}
--- backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
+++ backend-resources/src/main/java/com/sergio/auth/backend/resources/controllers/ResourceController.java
@@ -1,10 +1,23 @@
 package com.sergio.auth.backend.resources.controllers;
 
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class ResourceController {
+
+    /*@GetMapping("/messages")
+    public Authentication index(Principal principal) {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+        return authentication;
+    }*/
 
     @GetMapping("/messages")
     public String getMessages() {
