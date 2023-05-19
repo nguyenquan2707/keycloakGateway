96c775e585ca08e61d24493d1313f1b7084ba751
Quan Nguyen <vozermuonnam1@gmail.com>
2023-05-19 09:48:19 +0700

    Override convert authorities, it will using role instead of default scope

--- backend-resources/src/main/java/com/sergio/auth/backend/resources/config/SecurityConfig.java
+++ backend-resources/src/main/java/com/sergio/auth/backend/resources/config/SecurityConfig.java
@@ -1,9 +1,19 @@
 package com.sergio.auth.backend.resources.config;
 
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,10 +29,28 @@ public class SecurityConfig {
         http.mvcMatcher("/**")
                 .authorizeRequests()
                 .mvcMatchers("/**")
-                .access("hasAuthority('SCOPE_message.read')")
+                .hasRole("user")
+//                .access("hasAuthority('SCOPE_message.read')")
+
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(source -> new CustomJwtConfigure().convert(source));
         return http.build();
+    }
+
+    public static class CustomJwtConfigure implements Converter<Jwt, JwtAuthenticationToken> {
+        private static final String REALM = "realm_access";
+        private static final String ROLES = "roles";
+
+        @Override
+        public JwtAuthenticationToken convert(Jwt jwt) {
+            var tokenAttributes = jwt.getClaims();
+            var jsonObject = (JSONObject) tokenAttributes.get(REALM);
+            var roles = (JSONArray) jsonObject.get(ROLES);
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+            roles.forEach(role -> grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+            return new JwtAuthenticationToken(jwt, grantedAuthorities);
+        }
     }
 }
